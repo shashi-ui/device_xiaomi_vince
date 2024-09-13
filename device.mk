9c2300689ca7e0785276801293991494589cc910
Dibyaranjan Pradhan <61652361+dib12@users.noreply.github.com>
2024-09-13 18:42:11 +0530

    Vince: Build 64bit com.fingerprints.extension@1.0.so only

--- device.mk
+++ device.mk
@@ -34,7 +34,7 @@ PRODUCT_PACKAGES += \
 # Fingerprint
 PRODUCT_PACKAGES += \
     android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953 \
-    com.fingerprints.extension@1.0.vendor
+    com.fingerprints.extension@1.0.vendor:64
 
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
