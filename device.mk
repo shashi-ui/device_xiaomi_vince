ee81575ee3dfde60913a6b292460efd34c30ad8e
shashi-ui <shashiexplorer@gmail.com>
2024-10-10 13:20:06 +0530

    Update device.mk

--- device.mk
+++ device.mk
@@ -31,14 +31,6 @@ PRODUCT_PACKAGES += \
     android.hardware.ir@1.0-impl \
     android.hardware.ir@1.0-service
 
-# Fingerprint
-PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.1-service.xiaomi_msm8953 \
-    com.fingerprints.extension@1.0.vendor:64
-
-PRODUCT_COPY_FILES += \
-    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
-
 # Rootdir
 PRODUCT_PACKAGES += \
     init.recovery.qcom.rc
